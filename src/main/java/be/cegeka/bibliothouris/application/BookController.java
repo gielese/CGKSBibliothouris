7e70c11ec00789117ee14ce4e9e9d6c59344edce
DavidScheers <david.scheers@gmail.com>
2017-01-25 11:20:32 +0100

    Added bookcontroller

---
+++ src/main/java/be/cegeka/bibliothouris/application/BookController.java
@@ -0,0 +1,37 @@
+package be.cegeka.bibliothouris.application;
+
+import be.cegeka.bibliothouris.domain.books.Book;
+import be.cegeka.bibliothouris.domain.books.BookService;
+import be.cegeka.bibliothouris.domain.users.User;
+import be.cegeka.bibliothouris.domain.users.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    @Inject
+    private BookService bookService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public
+    @ResponseBody
+    List<Book> getBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public
+    @ResponseBody
+    void addBook(@RequestParam(value = "name") String name, @RequestParam(value = "ISBN") long ISBN) {
+        bookService.addBook(name, ISBN);
+    }
+
+}
